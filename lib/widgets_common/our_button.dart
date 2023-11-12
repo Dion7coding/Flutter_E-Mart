4847f3550b1713dac8525fc5a56d6bc625b7ab96
Darker621 <sahil.sajayan63@gmail.com>
2023-11-12 13:56:26 +0530

    New change

--- lib/widgets_common/our_button.dart
+++ lib/widgets_common/our_button.dart
@@ -1,11 +1,10 @@
 import 'package:emart_app/consts/consts.dart';
 
-Widget ourButton({onPress,color,textColor,title}) {
+Widget ourButton() {
   return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-          backgroundColor: color, padding: EdgeInsets.all(15)),
-      onPressed: () {
-        onPress;
-      },
-      child: title.text.color(textColor).fontFamily(bold).make());
+    style: ElevatedButton.styleFrom(
+      backgroundColor: blackColor,
+      padding: EdgeInsets.all(15)
+    ),
+      onPressed: () {}, child: login.text.white.fontFamily(bold).make());
 }
