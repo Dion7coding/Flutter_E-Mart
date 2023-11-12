310658e19ee16ed21fc1cfa740a942dd46b5b48d
Darker621 <sahil.sajayan63@gmail.com>
2023-11-12 15:34:00 +0530

    new change

--- lib/views/auth_screen/login_screen.dart
+++ lib/views/auth_screen/login_screen.dart
@@ -1,8 +1,12 @@
 import 'package:emart_app/consts/consts.dart';
+import 'package:emart_app/consts/lists.dart';
+import 'package:emart_app/views/auth_screen/signup_screen.dart';
 import 'package:emart_app/widgets_common/applogo_widget.dart';
 import 'package:emart_app/widgets_common/bg_widget.dart';
 import 'package:emart_app/widgets_common/custom_textfield.dart';
 import 'package:emart_app/widgets_common/our_button.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -11,6 +15,7 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return bgWidget(
         child: Scaffold(
+          resizeToAvoidBottomInset: false,
       body: Center(
         child: Column(
           children: [
@@ -27,16 +32,38 @@ class LoginScreen extends StatelessWidget {
                     alignment: Alignment.centerRight,
                     child: TextButton(
                         onPressed: () {}, child: forgetPass.text.make())),
-                        5.heightBox,
-                        // ourButton().box.width(context.screenWidth -100).make()
-                        ourButton(color: blackColor,title: login,textColor: whiteColor,onPress: (){}).box.width(context.screenWidth-100).make()
+                5.heightBox,
+                //Login Button
+                ourButton(
+                        color: blackColor,
+                        title: login,
+                        textColor: whiteColor,
+                        onPress: () {})
+                    .box
+                    .width(context.screenWidth - 120)
+                    .make(),
+
+                    5.heightBox,
+                    createNewAccount.text.color(fontGrey).make(),
+                    //sign-Up Button
+                    5.heightBox,
+                    ourButton(
+                        color: blackColor,
+                        title: signup,
+                        textColor: whiteColor,
+                        onPress: () {
+                          Get.to(()=>Signup());
+                        })
+                    .box
+                    .width(context.screenWidth - 120)
+                    .make(),      
               ],
             )
                 .box
                 .white
                 .rounded
-                .padding(EdgeInsets.all(1))
-                .width(context.screenWidth - 70)
+                .padding(EdgeInsets.all(16))
+                .width(context.screenWidth - 70).shadowSm
                 .make()
           ],
         ),
---
+++ lib/views/auth_screen/signup_screen.dart
@@ -0,0 +1,78 @@
+import 'package:emart_app/consts/consts.dart';
+import 'package:emart_app/widgets_common/applogo_widget.dart';
+import 'package:emart_app/widgets_common/bg_widget.dart';
+import 'package:emart_app/widgets_common/custom_textfield.dart';
+import 'package:emart_app/widgets_common/our_button.dart';
+
+class Signup extends StatelessWidget {
+  const Signup({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return bgWidget(
+        child: Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Column(
+          children: [
+            (context.screenHeight * 0.1).heightBox,
+            applogoWidget(),
+            20.heightBox,
+            "Join the  $appname".text.fontFamily(bold).white.size(18).make(),
+            40.heightBox,
+            Column(
+              children: [
+                customTextField(hint: NameHint, title: Name),
+                customTextField(hint: emailHint, title: email),
+                customTextField(hint: passwordHint, title: password),
+                customTextField(hint: passwordHint, title: retypePassword),
+                Row(
+                  children: [
+                    Checkbox(value: false, onChanged: (newValue) {}),
+                    10.widthBox,
+                    Expanded(
+                      child: RichText(
+                          text: const TextSpan(children: [
+                        TextSpan(
+                            text: "I agree to the ",
+                            style: TextStyle(fontFamily: bold, color: fontGrey)),
+                        TextSpan(
+                            text: termsAndConditions,
+                            style:
+                                TextStyle(fontFamily: bold, color: blackColor)),
+                        TextSpan(
+                            text: "&",
+                            style:
+                                TextStyle(fontFamily: bold, color: blackColor)),
+                        TextSpan(
+                            text: PrivacyPolicy,
+                            style:
+                                TextStyle(fontFamily: bold, color: blackColor)),
+                      ])),
+                    )
+                  ],
+                ),
+                5.heightBox,
+                ourButton(
+                        color: blackColor,
+                        title: signup,
+                        textColor: whiteColor,
+                        onPress: () {})
+                    .box
+                    .width(context.screenWidth - 120)
+                    .make(),
+              ],
+            )
+                .box
+                .white
+                .rounded
+                .padding(EdgeInsets.all(16))
+                .width(context.screenWidth - 70)
+                .shadowSm
+                .make()
+          ],
+        ),
+      ),
+    ));
+  }
+}
