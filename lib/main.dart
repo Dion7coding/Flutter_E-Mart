46bbd1a4e1691cec88110076a5e573ee1245b456
Darker621 <sahil.sajayan63@gmail.com>
2023-11-13 18:47:09 +0530

    new change

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:emart_app/views/splash/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'consts/consts.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -16,10 +19,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: appname,
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.transparent,
-        fontFamily: regular
-      ),   
-      home:const SplashScreen(),
+          scaffoldBackgroundColor: Colors.transparent, fontFamily: regular),
+      home: const SplashScreen(),
     );
   }
 }
