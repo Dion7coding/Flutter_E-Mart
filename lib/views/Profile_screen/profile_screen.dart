ebbcff489392aec8e6bcfdc72c4c730d6c99fe4c
Darker621 <sahil.sajayan63@gmail.com>
2023-11-13 16:09:39 +0530

    new commit

--- lib/views/Profile_screen/profile_screen.dart
+++ lib/views/Profile_screen/profile_screen.dart
@@ -13,6 +13,14 @@ class ProfileScreen extends StatelessWidget {
           padding: EdgeInsets.all(8),
           child: Column(
             children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: Icon(
+                  Icons.edit,
+                  color: whiteColor,
+                ),
+              ),
+              //users details section
               Row(
                 children: [
                   Icon(
@@ -20,17 +28,42 @@ class ProfileScreen extends StatelessWidget {
                     color: Colors.white,
                     size: 50,
                   ).box.make(),
+                  10.widthBox,
                   Expanded(
                       child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       "Dummy User".text.fontFamily(semibold).white.make(),
-                      
-                      "customer@example.com".text.fontFamily(semibold).white.make()
+                      "customer@example.com"
+                          .text
+                          .fontFamily(semibold)
+                          .white
+                          .make()
                     ],
-                  ))
+                  )),
+                  OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                          side: BorderSide(color: whiteColor)),
+                      onPressed: () {},
+                      child: "Logout".text.fontFamily(semibold).white.make())
+                ],
+              ),
+              20.heightBox,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  "00".text.fontFamily(bold).color(blackColor).size(16).make(),
+                  5.heightBox,
+                  "Items in Cart".text.color(blackColor).make()
                 ],
               )
+                  .box
+                  .white
+                  .rounded
+                  .width(context.screenWidth / 3.2)
+                  .height(60)
+                  .padding(EdgeInsets.all(4))
+                  .make()
             ],
           ),
         ),
