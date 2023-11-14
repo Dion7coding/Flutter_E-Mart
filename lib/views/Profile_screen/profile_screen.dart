6c5753d628b3637d719d36bc078612a5c0fdd367
Dion7coding <dionsijik@gmail.com>
2023-11-14 12:12:13 +0530

    new changes made

--- lib/views/Profile_screen/profile_screen.dart
+++ lib/views/Profile_screen/profile_screen.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:emart_app/consts/consts.dart';
 import 'package:emart_app/consts/lists.dart';
@@ -6,8 +8,6 @@ import 'package:emart_app/services/firestore_services.dart';
 import 'package:emart_app/views/Profile_screen/components/details_card.dart';
 import 'package:emart_app/views/Profile_screen/edit_screen.dart';
 import 'package:emart_app/views/auth_screen/login_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -15,46 +15,51 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-        body: StreamBuilder(
-          stream: FireStoreServices.getUser(currentUser!.uid),
-          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (!snapshot.hasData) {
-              return Center(
-                child: CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation(redColor)),
-              );
-            } else {
-              var data = snapshot.data!.docs[0];
+      backgroundColor: Colors.black,
+      body: StreamBuilder(
+        stream: FireStoreServices.getUser(currentUser!.uid),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(redColor),
+              ),
+            );
+          } else {
+            var data = snapshot.data!.docs[0];
 
-              return SafeArea(
-                child: Container(
-                  padding: EdgeInsets.all(8),
-                  child: Column(
-                    children: [
-                      Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: const Align(
-                              alignment: Alignment.topRight,
-                              child: Icon(
-                                Icons.edit,
-                                color: whiteColor,
-                              )).onTap(() {
-                            Get.to(() => const EditProfileScreen());
-                          })),
-                      //users details section
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          children: [
-                            Icon(
-                              Icons.person_2_outlined,
-                              color: Colors.white,
-                              size: 50,
-                            ).box.make(),
-                            10.widthBox,
-                            Expanded(
-                                child: Column(
+            // Assert that the two lists have the same length
+            assert(profileButtonsList.length == profileButtonIcon.length);
+
+            return SafeArea(
+              child: Container(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: Icon(
+                          Icons.edit,
+                          color: whiteColor,
+                        ),
+                      ).onTap(() {
+                        Get.to(() => const EditProfileScreen());
+                      }),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.person_2_outlined,
+                            color: Colors.white,
+                            size: 50,
+                          ).box.make(),
+                          10.widthBox,
+                          Expanded(
+                            child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 "Welcome"
@@ -68,77 +73,84 @@ class ProfileScreen extends StatelessWidget {
                                     .white
                                     .make()
                               ],
-                            )),
-                            OutlinedButton(
-                                style: OutlinedButton.styleFrom(
-                                    side: BorderSide(color: whiteColor)),
-                                onPressed: () async {
-                                  await Get.put(AuthController())
-                                      .signoutMethod(context: context);
-                                  Get.offAll(() => const LoginScreen());
-                                },
-                                child: "Logout"
-                                    .text
-                                    .fontFamily(semibold)
-                                    .white
-                                    .make())
-                          ],
-                        ),
-                      ),
-                      20.heightBox,
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          DetailsCard(
-                              count: data['cart_count'],
-                              title: "Items in Cart",
-                              width: context.screenWidth / 3.5),
-                          DetailsCard(
-                              count: data['wishlist_count'],
-                              title: "Wishlist",
-                              width: context.screenWidth / 3.5),
-                          DetailsCard(
-                              count: data['order_count'],
-                              title: "Your Orders",
-                              width: context.screenWidth / 3.5)
-                        ],
-                      ),
-                      30.heightBox,
-                      //Buttons section
-                      ListView.separated(
-                        shrinkWrap: true,
-                        separatorBuilder: (context, index) {
-                          return Divider(
-                            color: Vx.gray500,
-                          );
-                        },
-                        itemCount: profileButtonsList.length,
-                        itemBuilder: (context, index) {
-                          return ListTile(
-                            leading: Image.asset(
-                              profileButtonIcon[index],
-                              width: 24,
                             ),
-                            title: profileButtonsList[index]
+                          ),
+                          OutlinedButton(
+                            style: OutlinedButton.styleFrom(
+                              side: BorderSide(color: whiteColor),
+                            ),
+                            onPressed: () async {
+                              await Get.put(AuthController())
+                                  .signoutMethod(context: context);
+                              Get.offAll(() => const LoginScreen());
+                            },
+                            child: "Logout"
                                 .text
                                 .fontFamily(semibold)
-                                .color(darkFontGrey)
-                                .color(blackColor)
+                                .white
                                 .make(),
-                          );
-                        },
-                      )
-                          .box
-                          .white
-                          .rounded
-                          .padding(EdgeInsets.symmetric(horizontal: 16))
-                          .make()
-                    ],
-                  ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    20.heightBox,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        DetailsCard(
+                          count: data['cart_count'],
+                          title: "Items in Cart",
+                          width: context.screenWidth / 3.5,
+                        ),
+                        DetailsCard(
+                          count: data['wishlist_count'],
+                          title: "Wishlist",
+                          width: context.screenWidth / 3.5,
+                        ),
+                        DetailsCard(
+                          count: data['order_count'],
+                          title: "Your Orders",
+                          width: context.screenWidth / 3.5,
+                        ),
+                      ],
+                    ),
+                    30.heightBox,
+                    // Buttons section
+                    ListView.separated(
+                      shrinkWrap: true,
+                      separatorBuilder: (context, index) {
+                        return Divider(
+                          color: Vx.gray500,
+                        );
+                      },
+                      itemCount: profileButtonsList.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: Image.asset(
+                            profileButtonIcon[index],
+                            width: 24,
+                          ),
+                          title: profileButtonsList[index]
+                              .text
+                              .fontFamily(semibold)
+                              .color(darkFontGrey)
+                              .color(blackColor)
+                              .make(),
+                        );
+                      },
+                    )
+                        .box
+                        .white
+                        .rounded
+                        .padding(EdgeInsets.symmetric(horizontal: 16))
+                        .make(),
+                  ],
                 ),
-              );
-            }
-          },
-        ));
+              ),
+            );
+          }
+        },
+      ),
+    );
   }
 }
